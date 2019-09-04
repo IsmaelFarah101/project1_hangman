3b0d75e573123501c4c28cee88c30657ba162e29
IsmaelFarah101 <ismaelomermohamed1@gmail.com>
2019-09-04 06:11:16 -0500

    Added Comments

--- hangman.py
+++ hangman.py
@@ -1,11 +1,14 @@
+##this is the class that creates the hangman game
 class hangman:
     def __init__(self, word):
+        ##this is where we initialize the instance variables
         self.word = word
         self.chances = len(word) + 4
-
+        ## this returns the chances left
     def __str__(self):
         return f'You have {self.chances} chances'
-    
+    ##this allows the user to guess the word if the user guesses correctly it returns true and deletes the letter or letters from the word
+    # if the user guesses wrongly it returns false 
     def guess(self,guess):
         for letter in self.word:
             if guess in self.word:
@@ -15,11 +18,13 @@ class hangman:
             else:
                 self.chances = self.chances - 1
                 return False
+    ##this returns the boolean of the amount of letters left in the word
     def empty(self):
         if len(self.word) <= 1:
             return False
         else:
             return True
+    ##this returns the boolean of the amount of chances left
     def chance(self):
         if self.chances <= 1:
             return False
--- main.py
+++ main.py
@@ -1,14 +1,21 @@
+##importing the nessacaty modules
 import random
 from hangman import hangman
+##opening a file of random words for the user to guess
 wordFile = open('words.txt').read().splitlines()
+##getting the word
 word = random.choice(wordFile)
+##initiating the game
 game = hangman(word)
-print(game)
+##I printed the word just to check if the logic worked 
 print(word)
 guess = input('Enter a single character in the word: ')
 while guess:
+    ##checks if the user has any chances left
     if game.chance():
+        ##checks if the user successfully guesed the word
         if game.empty():
+            ##checks if the user correctly guesses a letter
             if game.guess(guess[0]):
                 print('You got one')
                 print(game)
